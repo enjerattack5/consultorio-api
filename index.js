8f74aa03da77b4f33a865fe7206163665834a193
Giovani-jefe_Grupo <enjerattack5@gmail.com>
2026-05-28 22:01:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose")
 const app = require("./app")
 
-// Toma la variable global del entorno de Vercel
 const MONGO_URI = process.env.MONGO_URI
 
 mongoose.connect(MONGO_URI)
 .then(() => console.log("Base de datos conectada exitosamente"))
 .catch((error) => console.error("Error al conectar la base de datos:", error))
 
-// El puerto solo se levanta si estás corriendo el proyecto localmente
 if (process.env.NODE_ENV !== 'production') {
     const PORT = process.env.PORT || 4000
     app.listen(PORT, () => {
@@ -16,5 +14,4 @@ if (process.env.NODE_ENV !== 'production') {
     })
 }
 
-// Esto es lo que Vercel necesita en la raíz para mapear tus rutas
 module.exports = app
